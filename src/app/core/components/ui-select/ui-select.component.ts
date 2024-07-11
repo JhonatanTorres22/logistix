468b4db9ee80588671a16161262fe27324f44f37
Angemar <angemar@autonomadeica.edu.pe>
2024-07-11 17:04:37 -0500

    ENVIAR MENSAJE Y VALIDAR BOTON

--- src/app/core/components/ui-select/ui-select.component.ts
+++ src/app/core/components/ui-select/ui-select.component.ts
@@ -3,6 +3,7 @@ import { Component, EventEmitter, Input, OnInit, Output, Self } from '@angular/c
 import { ControlValueAccessor, NgControl, FormControl } from '@angular/forms';
 import { SharedModule } from 'src/app/demo/shared/shared.module';
 import { UiSelect } from './ui-select.interface';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'ui-select',
@@ -14,11 +15,9 @@ import { UiSelect } from './ui-select.interface';
 export class UiSelectComponent implements ControlValueAccessor, OnInit{
 
   constructor(
-    @Self() private ngControl: NgControl
-  ) {
-    if( ngControl ) {
-      this.ngControl.valueAccessor = this
-    }
+    @Self() public ngControl: NgControl
+  ) { 
+    if( ngControl ) { this.ngControl.valueAccessor = this  }
   }
 
   hide: boolean = true;
@@ -31,19 +30,23 @@ export class UiSelectComponent implements ControlValueAccessor, OnInit{
   @Output() onChange = new EventEmitter();
   @Input() appearance = 'outline';
   @Input() image: string = '';
-  
+  @Output() selectionChange = new EventEmitter<MatSelectChange>();
   formControl!: FormControl
   
   ngOnInit(): void {
     this.formControl = this.ngControl.control as FormControl
   }
 
-  
+  onSelectionChange(event: any) {
+    this.selectionChange.next(event);
+  }
 
   writeValue(obj: any): void {
     // throw new Error('Method not implemented.');
+    // this._renderer.setProperty(this._elementRef.nativeElement, 'value', value);
   }
   registerOnChange(fn: any): void {
+    // this._onChange = fn;
     // throw new Error('Method not implemented.');
   }
   registerOnTouched(fn: any): void {
@@ -52,6 +55,5 @@ export class UiSelectComponent implements ControlValueAccessor, OnInit{
   setDisabledState?(isDisabled: boolean): void {
     // throw new Error('Method not implemented.');
   }
- 
   
 }
